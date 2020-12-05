76bc96b8d4819193043eeea449a4e306a86aed22
imdgr886 <thrall.chen@gmail.com>
2020-12-05 19:10:55 +0800

    fix-order-volume-int

--- js/huobipro.js
+++ js/huobipro.js
@@ -1470,7 +1470,7 @@ module.exports = class huobipro extends Exchange {
             request = {
                 'contract_code': symbol,
                 'order_price_type': type,
-                'volume': amount,
+                'volume': int (amount),
                 'direction': side,
             };
             if (type === 'market') {
